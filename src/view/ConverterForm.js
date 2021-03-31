d08c9bd6641d842a286bfa3b38a08a7bc0934565
Gloria Tan <sygloria.tan@gmail.com>
2021-04-01 01:11:23 +0800

    feat: currency of the day

--- src/view/ConverterForm.js
+++ src/view/ConverterForm.js
@@ -6,12 +6,33 @@ import Select from '@material-ui/core/Select'
 import { TextField } from '@material-ui/core'
 import SwapHorizIcon from '@material-ui/icons/SwapHoriz'
 import IconButton from '@material-ui/core/IconButton'
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 import axios from 'axios'
+import get from 'lodash/get'
+
 
 const useStyles = makeStyles((theme) => ({
     formControl: {
       margin: theme.spacing(1),
       minWidth: 120,
+    },
+    root: {
+        minWidth: 275,
+      },
+    bullet: {
+        display: 'inline-block',
+        margin: '0 2px',
+        transform: 'scale(0.8)',
+    },
+    title: {
+        fontSize: 14,
+    },
+    pos: {
+        marginBottom: 12,
     },
   }));
 
@@ -19,7 +40,7 @@ const useStyles = makeStyles((theme) => ({
 const ConverterForm = () => {
     const classes = useStyles()
 
-    const axios = require('axios')
+    // const axios = require('axios')
 
     const currencyCode = require('currency-codes')
 
@@ -29,11 +50,13 @@ const ConverterForm = () => {
 
     let [toCode, setToCode] = useState('')
 
+    let [rate, setRate] = useState(0)
+
     
     
-    useEffect(() => {
-        getUser()
-    })
+    // useEffect(() => {
+    //     getUser()
+    // })
 
 
     let cList = currencyList.length > 0
@@ -53,16 +76,27 @@ const ConverterForm = () => {
         setToCode(event.target.value)
     }
 
-    
-
-    async function getUser() {
+    async function handleChange() {
         try {
-          const response = await axios.get(`https://free.currconv.com/api/v7/convert?q=${fromCode}_${toCode}&compact=ultra&apiKey=${process.env.REACT_APP_API_KEY}`)
-          console.log(response)
-        } catch (error) {
-          console.error(error);
-        }
-      }
+            const response = await axios.get(`https://free.currconv.com/api/v7/convert?q=${fromCode}_${toCode}&compact=ultra&apiKey=${process.env.REACT_APP_API_KEY}`)
+            // console.log(get(response, ['data', `${fromCode}_${toCode}`]))
+            console.log(get(response, ['data', `${fromCode}_${toCode}`]))
+            setRate(get(response, ['data', `${fromCode}_${toCode}`]))
+          } catch (error) {
+            console.error(error);
+          }
+    }
+
+    
+    // async function getUser() {
+    //     try {
+    //       const response = await axios.get(`https://free.currconv.com/api/v7/convert?q=${fromCode}_${toCode}&compact=ultra&apiKey=${process.env.REACT_APP_API_KEY}`)
+    //     //   console.log(response)
+    //       setRate(response)
+    //     } catch (error) {
+    //       console.error(error);
+    //     }
+    //   }
 
     return(
         <>
@@ -83,9 +117,9 @@ const ConverterForm = () => {
                     </IconButton>
                 </label>
             </FormControl>
-            <FormControl className={classes.formControl}>
+            {/* <FormControl className={classes.formControl}>
                 <TextField id="standard-basic" defaultValue="1" label="Amount" />
-            </FormControl>
+            </FormControl> */}
             <FormControl className={classes.formControl}>
                 <InputLabel htmlFor="grouped-native-select">To</InputLabel>
                 <Select native defaultValue="" id="grouped-native-select" onChange={handleToChange}>
@@ -93,6 +127,28 @@ const ConverterForm = () => {
                 {cList}
                 </Select>
             </FormControl>
+            <Button variant="contained" color="primary" onClick={handleChange}>
+                Convert
+            </Button>
+            <Card className={classes.root}>
+            <CardContent>
+                <Typography className={classes.title} color="textSecondary" gutterBottom>
+                Currency of the Day
+                </Typography>
+                <Typography variant="h5" component="h2">
+                    {/* {get(getUser(), ['data', `${fromCode}_${toCode}`])} */}
+                    {rate}
+                </Typography>
+                {/* <Typography className={classes.pos} color="textSecondary">
+                adjective
+                </Typography>
+                <Typography variant="body2" component="p">
+                well meaning and kindly.
+                <br />
+                {'"a benevolent smile"'}
+                </Typography> */}
+            </CardContent>
+            </Card>
        </>
     )
 }
